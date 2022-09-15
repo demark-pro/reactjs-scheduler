d2d10caeba28efdf4343ab2d88cf2e06b7509731
demark-pro <me@demark.pro>
2022-09-16 01:54:23 +0300

    add base template

--- src/components/Scheduler/Scheduler.jsx
+++ src/components/Scheduler/Scheduler.jsx
@@ -1,3 +1,12 @@
+import { createRef } from "react";
+import styles from "./Scheduler.module.css";
+
 export default function Scheduler() {
-  return <></>;
+  const containerRef = createRef(null);
+
+  return (
+    <div ref={containerRef} className={styles.container}>
+      content
+    </div>
+  );
 }
